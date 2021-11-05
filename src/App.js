00e87449751d4d9e6c0d262ada652109937a1aaf
Tamara Glushchenko <gluschenkotoma@ukr.net>
2021-11-05 12:10:53 +0200

    section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-function App() {
-  return (
-    <>
-      <h1>Hello!</h1>
-    </>
-  );
-}
+import React, { Component } from "react";
 
+import Section from "./components/Section";
+
+class App extends Component {
+  state = { good: 0, neutral: 0, bad: 0 };
+  countTotalFeedback() {}
+  countPositiveFeedbackPercentage() {}
+  render() {
+    return (
+      <>
+        <Section title="Please leave feedback"></Section>
+        <Section title="Statistics"></Section>
+      </>
+    );
+  }
+}
 export default App;
